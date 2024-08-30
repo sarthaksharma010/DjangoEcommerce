8e450dced2a5f24c794e370c5c043e68aae40acd
Sarthak Sharma <sarthakarthsharma@gmail.com>
2024-08-31 00:24:04 +0530

    abc

---
+++ ecommercesite/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./templates/**/*.html',
+    './products/templates/**/*.html',
+    './products/templates/products/product/details.html',
+    './cart/templates/cart/detail.html', 
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
